2677893f54a64dfad81544db3642ac1059869ad5
Tom Saccasyn <student@students-mac.local>
2018-04-24 11:24:06 +0200

    testcommit

---
+++ MobileAppsIOSWerkstuk/MobileAppsIOSWerkstuk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobileAppsIOSWerkstuk
+//
+//  Created by student on 24/04/2018.
+//  Copyright © 2018 EHB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobileAppsIOSWerkstuk/MobileAppsIOSWerkstuk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobileAppsIOSWerkstuk
+//
+//  Created by student on 24/04/2018.
+//  Copyright © 2018 EHB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
