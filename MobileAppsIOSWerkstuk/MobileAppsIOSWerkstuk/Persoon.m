9ffbcf6da3474781e92c640d6131f17aea183e30
Tom Saccasyn <student@students-mac.local>
2018-04-30 16:21:44 +0200

    layout werkstuk gemaakt buiten de segues

---
+++ MobileAppsIOSWerkstuk/MobileAppsIOSWerkstuk/Persoon.m
@@ -0,0 +1,14 @@
+//
+//  Persoon.m
+//  MobileAppsIOSWerkstuk
+//
+//  Created by student on 27/04/2018.
+//  Copyright © 2018 EHB. All rights reserved.
+//
+
+#import "Persoon.h"
+
+
+@implementation Persoon
+
+@end
